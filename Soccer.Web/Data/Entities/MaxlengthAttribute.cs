347db11e376edab58a175f001e7972a875709494
jacfc <jacfc@DESKTOP-QM4K6Q5>
2023-05-31 14:55:54 -0500

    adicionamos team controler

---
+++ Soccer.Web/Data/Entities/MaxlengthAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Soccer.Web.Data.Entities
+{
+    internal class MaxlengthAttribute : Attribute
+    {
+        private int v;
+
+        public MaxlengthAttribute(int v, string ErrorMessage)
+        {
+            this.v = v;
+            this.ErrorMessage = ErrorMessage;
+        }
+
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ Soccer.Web/Data/Entities/TeamEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Data.Entities
+{
+    public class TeamEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "the {0} field can not have more than {1} characters.") ]
+        [Required  (ErrorMessage = " the field {0} is mandatory") ] 
+        public string Name { get; set; }
+        public string LogoPath { get; set; }
+    }
+}
